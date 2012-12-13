38daa302e2b0c1831e6e68f2f951f816670f8571
Judson Mitchell <judsonmitchell@gmail.com>
2012-12-13 23:50:22 +0000

    Begin api calls

--- api/index.php
+++ api/index.php
@@ -10,42 +10,70 @@
 */
 
 require_once('../db.php');
-
-function handle_request()
+error_reporting(E_ALL);
+function handle_request($dbh,$params)
 {
-    $request_type = $_SERVER['REQUEST_METHOD'];
+    //print_r($params);die;
+    $books = array('RS','CE','CCH','LAC');//supported sections
+    
+    if (in_array(strtoupper($params[0]), $books))
+    {
+        //if last array element is all numeric characters,
+        //we are doing are lookup by statute number.  Otherwise,
+        //we are doing a text search
+        if (ctype_digit(end($params)))
+        {
+            $book = strtoupper($params[0]);
+            //strip the book title (RS,CE, etc), leave only section numbers
+            array_shift($params);
+            $sections = null;
+            foreach ($params as $param) {
 
-    switch ($request_type) {
-        case 'PUT':
-            // code...
-            break;
-        case 'DELETE':
-            // code...
-            break;
-        case 'POST':
-            // code...
-            break;
-        case 'GET':
-            // code...
-            break;
-        default:
-            // code...
-            break;
+                $sections .= " " .  str_pad($param, 6, '0', STR_PAD_LEFT);
+                
+            }
+            
+            $sortcode = $book . $sections;
+            $q = $dbh->prepare("select * from laws where sortcode like ?");
+            $q->bindParam(1, $sortcode);
+            $q->execute();
+            $result = $q->fetch(PDO::FETCH_ASSOC);
+            $error = $q->errorInfo();
+            //if ($error[1]) { 
+            //    return array('status' => '500 Internal Server Error','message' => $error[2]);}
+            //if ($result->rowCount() < 1) {
+            //    return array('status' => '404 Not Found','message' => 'No documents matched your request');}
+            //else {
+            //    $result['status'] = '200 OK';
+            //    return $result;
+            //}
+            return $result;
+        }
     }
+} 
 
-
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') //we need only support GET
+{
+    header("{$_SERVER['SERVER_PROTOCOL']} 405 Method Not Allowed");
+    die();
 }
-if (isset($_SERVER['QUERY_STRING'])) {
-    parse_str($_SERVER['QUERY_STRING'], $parameters);
-    print_r($parameters);
-    echo "shit";
+
+//Get the parameters
+
+parse_str($_SERVER['QUERY_STRING'],$query_string); //check for query string
+
+if (count($query_string) > 0)
+{
+    $params = $query_string;
+}
+else
+{
+    $params = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 }
 
-$verb = $_SERVER['REQUEST_METHOD'];
-$url_elements = explode('/', $_SERVER['PATH_INFO']);
-
-echo $verb;
-print_r($url_elements);
+$result = handle_request($dbh,$params);
+//header("{$_SERVER['SERVER_PROTOCOL']}" . $result['status']);
+echo json_encode($result);
 
 //What does an API request look like?:
 
