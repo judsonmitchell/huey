418c7d59310406901d20251acfe695c893a1d80c
Judson Mitchell <judsonmitchell@gmail.com>
2012-12-23 14:59:47 +0000

    Add site index.php to repo

---
+++ site/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Huey - A Search Engine for Louisiana Statutory Laws</title>
+        <meta name="description" content="Huey - A Search Engine for Louisiana Statutory Laws">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <style>
+            body {
+                padding-top: 60px;
+                padding-bottom: 40px;
+            }
+        </style>
+        <link rel="stylesheet" href="css/bootstrap-responsive.min.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link href="http://fonts.googleapis.com/css?family=Cagliostro" rel="stylesheet" type="text/css">
+
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div class="container">
+            <div class = "hero-unit hero-unit-main">
+                <h1><img src="img/huey2.jpg">Huey</h1> 
+                <h3>A Search Engine for Louisiana Statutory Law</h3>
+                <input type="text" class="huey-search" placeholder="Enter Search Term">
+            </div> 
+            <div id = "results">
+            
+            </div>
+            <script id="each-template" type="text/x-handlebars-template"> 
+                {{#each laws }}
+                    <h3><a href="#" class="detail" docid={{docid}}>{{title}}</a></h3>
+                    <h4>{{description}}</h4>
+                    <div>{{{law_text}}}</div>
+                {{/each}}
+            </script>
+            <script id="error-template" type="text/x-handlebars-template"> 
+                <div class= "alert alert-error">
+                <a class="close" data-dismiss="alert">×</a> 
+                {{message}}
+                </div>
+            </script>
+
+        </div> <!-- /container -->
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/handlebars.js/1.0.rc.1/handlebars.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
+        <script src="js/vendor/bootstrap.min.js"></script>
+        <script src="js/vendor/html4+html5/jquery.history.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+        <script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+    </body>
+</html>
