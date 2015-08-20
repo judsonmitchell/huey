0665e49a38bc1ea49b177a956cc3f99d55514f9c
Judson Mitchell <judsonmitchell@gmail.com>
2015-08-20 12:34:56 -0500

    First working iteration of 2015 scraper

--- scraper/scraper.php
+++ scraper/scraper.php
@@ -89,6 +89,55 @@ function clean_sortcodes($val)
 
 }
 
+function make_sort_code($val){
+
+    /*
+    Possible parts:
+        [book] [title]:[section].[subsection]
+    problem is we can also have:
+        [book] [title].[subsection]
+    */
+    $parts = explode(' ', $val);
+    $book = $parts[0];
+    $dot = strrpos($parts[1], '.'); 
+    $colon = strrpos($parts[1], ':');
+    if ($dot){
+        $subsection = substr($parts[1],$dot + 1); 
+    } else {
+        $subsection = null;
+    }
+
+    if ($colon && $dot){
+        $title = substr($parts[1],0, $colon); 
+        $length = $dot - $colon -1; 
+        $section = substr($parts[1], $colon + 1, $length);
+    } else if ($colon){
+        $title = substr($parts[1],0, $colon); 
+        $section = substr($parts[1], $colon +1);  
+        //$length = $dot - $colon -1; 
+        //$section = substr($parts[1], $colon + 1, $length);
+    } else if ($dot){
+        $title = substr($parts[1],0, $dot); 
+        $section = null; 
+    } else {
+        $title = $parts[1]; 
+        $section = null;
+    }
+
+    $sortcode = $book . ' ' . 
+    str_pad($title, 6, '0', STR_PAD_LEFT); 
+    if ($section){
+        $sortcode .= ' ' . str_pad($section, 6, '0', STR_PAD_LEFT); 
+    }
+    if ($subsection){
+        $sortcode .= ' ' . str_pad($subsection, 6, '0', STR_PAD_LEFT);
+    }
+    return $sortcode;
+}
+echo make_sort_code('RS 14:103.1') . "\n";
+echo make_sort_code('CC 103.1') . "\n";
+echo make_sort_code('CC 15:143') . "\n";
+echo make_sort_code('CC 4512');die;
 echo "Scraping...this could take a while....";
 $counter = 0; //number of laws successfully scraped
 $errors = 0; //db errors
@@ -99,45 +148,44 @@ $docs = 0; //number of urls touched
 //tell the lowest id is around 66000 and the highest around 750000 
 //$min = 66000;
 //$max = 750000;
-// what ran 10-4 $min = 366646;
-// $max = 463819; new max as of 10/2014 should be 919602
-$min = 271526;
+$min = 108528;
 $max = 919602;
 
 for ($min; $min <= $max; $min++) {
 
-    $law = file_get_html('http://legis.la.gov/lss/newWin.asp?doc=' . $min);
+    $law = file_get_html('https://www.legis.la.gov/legis/LawPrint.aspx?d=' . $min);
 
     if (is_object($law)) {
 
         $docs++; //url has been hit
 
-        if (!$law->find('html',0)) //Server returns 'file not found'
-        {
+        if ($law->find('div[id=divError]')) { //Server returns 'file not found'
             $law->clear(); 
             unset($law);
-        }
-        else
-        {
+        } else {
             //Parse meta tags
-            $meta = array();
-            foreach($law->find('meta') as $item) {
-                $meta[$item->name] = $item->content; 
-            }
+            //$meta = array();
+            //foreach($law->find('meta') as $item) {
+            //    $meta[$item->name] = $item->content; 
+            //}
 
             //In the revised statutes, the meta tags contain the law title; in 
             //the others, there is no such tag.  So parse the <title> tag
-            $title = array();
-            foreach ($law->find('title') as $item) {
-                $title = $item->innertext;
-            }
+            //$title = array();
+            //foreach ($law->find('title') as $item) {
+                //$title = $item->innertext;
+            //}
 
+            $get_title = $law->find('span[id=LabelName]');
+            $title = $get_title[0]->innertext;
             //Get the entire body of the law; will use later when applying diff
             //to see if there has been a change
-            foreach ($law->find('body') as $b) {
-                $body = $b->innertext;
-            }
+            //foreach ($law->find('body') as $b) {
+                //$body = $b->innertext;
+            //}
 
+            $get_body = $law->find('div[id=divLaw]');
+            $body = $get_body[0]->innertext;
             //Strip all class and align attributes from p http://stackoverflow.com/a/3026111/49359
             $body_html = str_get_html($body);
             foreach ( $body_html->find('p') as $value ){
@@ -151,37 +199,46 @@ for ($min; $min <= $max; $min++) {
             //if it's the start of the chapter, you get chapter name instead; these
             //are, however, aligned center, so just find the first paragraph that is
             //aligned justify
-            $first_para = $law->find('p[align="justify"]',0);
-            if ($first_para)
-            {
-                $alt_description = explode('&nbsp;',$first_para->innertext);
-            }
+            //$first_para = $law->find('p[align="justify"]',0);
+            //if ($first_para)
+            //{
+                //$alt_description = explode('&nbsp;',$first_para->innertext);
+            //}
 
-            if (isset($meta['description']))
-            {
-                $description = $meta['description'];
-            }
-            elseif (isset($alt_description[1]))
-            {
-                $description = $alt_description[1];
-            }
-            else
-            {
-                $description = ''; //all else fails
-            }
+            //if (isset($meta['description']))
+            //{
+                //$description = $meta['description'];
+            //}
+            //elseif (isset($alt_description[1]))
+            //{
+                //$description = $alt_description[1];
+            //}
+            //else
+            //{
+                //$description = ''; //all else fails
+            //}
 
-            //Deal with inconsistent case of sortcode meta tag;
-            //sometimes it's capitalized, sometimes not
-            if (isset($meta['sortcode']))
-            {
-                $sortcode = clean_sortcodes($meta['sortcode']);    
-            }
-            else
-            {
-                $sortcode = clean_sortcodes($meta['Sortcode']);    
-            }
+            ////Deal with inconsistent case of sortcode meta tag;
+            ////sometimes it's capitalized, sometimes not
+            //if (isset($meta['sortcode']))
+            //{
+                //$sortcode = clean_sortcodes($meta['sortcode']);    
+            //}
+            //else
+            //{
+                //$sortcode = clean_sortcodes($meta['Sortcode']);    
+            //}
 
+            $get_description = $law->find('span[id=LabelDocument] p',0);
+            $description = $get_description->plaintext;
+
+            $sortcode = make_sort_code($title);
             //Put data in db
+            echo "docid: " . $min . "\n";
+            echo "title: " . $title . "\n";
+            echo "description: " . $description . "\n";
+            echo "sortcode: " . $sortcode . "\n";
+            echo "======================";
             $q = $dbh->prepare("INSERT INTO `laws` (`id`, `docid`, `title`,
             `description`, `sortcode`, `law_text`,`last_scraped`) 
             VALUES (NULL, :docid, :title, :description, :sortcode, :law_text, 
@@ -195,7 +252,7 @@ for ($min; $min <= $max; $min++) {
                 ':law_text' => $body_html
                 );
 
-            $q->execute($data);
+            //$q->execute($data);
 
             $error = $q->errorInfo();
             if ($error[1])
