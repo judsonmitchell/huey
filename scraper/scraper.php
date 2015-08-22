a6522f61740ade175a23b11e005ab5b6c8960936
Judson Mitchell <judsonmitchell@gmail.com>
2015-08-22 13:53:00 -0500

    Update scraper to handle 2015 changes to Legislature's html

--- scraper/scraper.php
+++ scraper/scraper.php
@@ -3,13 +3,15 @@
  * Huey - An API for Louisiana Statutory Laws
  *
  * This is the scraper which retrieves the laws
- * from the Legislature's database.
+ * from the Legislature's database. It has been
+ * updated to reflect changes to the Legislature's 
+ * html in 2015
  * 
  * Uses PHP Simple HTML DOM Parser:
  * http://simplehtmldom.sourceforge.net/
  *
  * @author Judson Mitchell <judsonmitchell@gmail.com>
- * @copyright 2012 Judson Mitchell, Three Pipe Problem, LLC
+ * @copyright 2012, 2015 Judson Mitchell, Three Pipe Problem, LLC
  * @url https://github.com/judsonmitchell/huey
  * @license MIT
  */
@@ -22,81 +24,9 @@ error_log( "Huey scraper error log:" );
 require_once('../db.php');
 require_once('simple_html_dom.php');
 
-$time_start = microtime(true);
-
-//Function to deal with anomalies in sortcode; remove any data
-//which is unncessary or which destroys the sort
-function clean_sortcodes($val)
-{
-    //make sure sortcode is six digits
-    $parts = explode(" ",$val);
-    array_walk($parts, function(&$p){
-        if (ctype_digit($p)) {
-            $p = str_pad($p, 6, '0', STR_PAD_LEFT);
-        }
-    });
-
-    //strip out cruft
-    switch ($val) {
-        case  substr_count($val,'RS') > 1: //revised statutes; duplicate rs line
-            $sortcode = substr($val,10);    
-            return $sortcode;
-            break;
-        case  substr_count($val,'CE') > 1: //code of evidence; duplicate "CE"
-            $sortcode = substr($val,3);    
-            return $sortcode;
-            break;
-        case  strstr($val,'CC  000200'): //civil code; mysterious "000200" 
-            $sortcode = str_replace('CC  000200','CC',$val);    
-            return $sortcode;
-            break;
-        case  substr_count($val,'CHC') > 1: //children's code; duplicate "CHC"
-            $sortcode = substr($val,4);    
-            return $sortcode;
-            break;
-        case  substr_count($val,'CCP') > 1: //code of civil proc.; duplicate "CCP"
-            $sortcode = substr($val,4);    
-            return $sortcode;
-            break;
-        case  substr_count($val,'CCRP') > 1: //code of criminal proc; duplicate "CCRP"
-            $sortcode = substr($val,5);    
-            return $sortcode;
-            break;
-        case  substr_count($val,'CONST') > 1: //constitution; duplicate "CONST"
-            $sortcode = substr($val,13);    
-            return $sortcode;
-            break;
-        case  substr_count($val,'LAC') > 1: //admin code;duplicate "LAC"
-            $sortcode = substr($val,11);    
-            return $sortcode;
-            break;
-        case  substr_count($val,'CA') > 1: //constit. amends; duplicate "CA" 
-            $sortcode = substr($val,3);    
-            return $sortcode;
-            break;
-        case  substr_count($val,'ERC') > 1: //ERC?; duplicate "ERC" 
-            $sortcode = substr($val,4);    
-            return $sortcode;
-            break;
-        case  substr_count($val,'CJP') > 1: //duplicate "CJP" 
-            $sortcode = substr($val,4);    
-            return $sortcode;
-            break;
-        default:
-            return $val;
-            break;
-    }
-
-}
 
 function make_sort_code($val){
 
-    /*
-    Possible parts:
-        [book] [title]:[section].[subsection]
-    problem is we can also have:
-        [book] [title].[subsection]
-    */
     $parts = explode(' ', $val);
     $book = $parts[0];
     $dot = strrpos($parts[1], '.'); 
@@ -134,11 +64,9 @@ function make_sort_code($val){
     }
     return $sortcode;
 }
-echo make_sort_code('RS 14:103.1') . "\n";
-echo make_sort_code('CC 103.1') . "\n";
-echo make_sort_code('CC 15:143') . "\n";
-echo make_sort_code('CC 4512');die;
+
 echo "Scraping...this could take a while....";
+$time_start = microtime(true);
 $counter = 0; //number of laws successfully scraped
 $errors = 0; //db errors
 $docs = 0; //number of urls touched
@@ -148,7 +76,7 @@ $docs = 0; //number of urls touched
 //tell the lowest id is around 66000 and the highest around 750000 
 //$min = 66000;
 //$max = 750000;
-$min = 108528;
+$min = 66000;
 $max = 919602;
 
 for ($min; $min <= $max; $min++) {
@@ -159,33 +87,18 @@ for ($min; $min <= $max; $min++) {
 
         $docs++; //url has been hit
 
-        if ($law->find('div[id=divError]')) { //Server returns 'file not found'
+        //Legislature's version of a 404 ;) 
+        if ($law->find('div[id=divError]') || $law->find('a[id=ctl00_MainMenu_SkipLink]')) {
             $law->clear(); 
             unset($law);
         } else {
-            //Parse meta tags
-            //$meta = array();
-            //foreach($law->find('meta') as $item) {
-            //    $meta[$item->name] = $item->content; 
-            //}
-
-            //In the revised statutes, the meta tags contain the law title; in 
-            //the others, there is no such tag.  So parse the <title> tag
-            //$title = array();
-            //foreach ($law->find('title') as $item) {
-                //$title = $item->innertext;
-            //}
 
             $get_title = $law->find('span[id=LabelName]');
             $title = $get_title[0]->innertext;
-            //Get the entire body of the law; will use later when applying diff
-            //to see if there has been a change
-            //foreach ($law->find('body') as $b) {
-                //$body = $b->innertext;
-            //}
 
-            $get_body = $law->find('div[id=divLaw]');
+            $get_body = $law->find('span[id=LabelDocument]');
             $body = $get_body[0]->innertext;
+
             //Strip all class and align attributes from p http://stackoverflow.com/a/3026111/49359
             $body_html = str_get_html($body);
             foreach ( $body_html->find('p') as $value ){
@@ -193,82 +106,53 @@ for ($min; $min <= $max; $min++) {
                 $value->align = null;
             }
 
-            //generate an alternative description if meta does not have it
-            //Having to find the align attribute is a special bit of fun; 99%
-            //of the time, the first paragraph has the description; but sometimes,
-            //if it's the start of the chapter, you get chapter name instead; these
-            //are, however, aligned center, so just find the first paragraph that is
-            //aligned justify
-            //$first_para = $law->find('p[align="justify"]',0);
-            //if ($first_para)
-            //{
-                //$alt_description = explode('&nbsp;',$first_para->innertext);
-            //}
-
-            //if (isset($meta['description']))
-            //{
-                //$description = $meta['description'];
-            //}
-            //elseif (isset($alt_description[1]))
-            //{
-                //$description = $alt_description[1];
-            //}
-            //else
-            //{
-                //$description = ''; //all else fails
-            //}
-
-            ////Deal with inconsistent case of sortcode meta tag;
-            ////sometimes it's capitalized, sometimes not
-            //if (isset($meta['sortcode']))
-            //{
-                //$sortcode = clean_sortcodes($meta['sortcode']);    
-            //}
-            //else
-            //{
-                //$sortcode = clean_sortcodes($meta['Sortcode']);    
-            //}
-
             $get_description = $law->find('span[id=LabelDocument] p',0);
             $description = $get_description->plaintext;
 
             $sortcode = make_sort_code($title);
-            //Put data in db
-            echo "docid: " . $min . "\n";
-            echo "title: " . $title . "\n";
-            echo "description: " . $description . "\n";
-            echo "sortcode: " . $sortcode . "\n";
-            echo "======================";
-            $q = $dbh->prepare("INSERT INTO `laws` (`id`, `docid`, `title`,
-            `description`, `sortcode`, `law_text`,`last_scraped`) 
-            VALUES (NULL, :docid, :title, :description, :sortcode, :law_text, 
-            CURRENT_TIMESTAMP);");
-    
-            $data = array(
-                ':docid' => $min,
-                ':title' => $title,
-                ':description' => $description,
-                ':sortcode' => $sortcode,
-                ':law_text' => $body_html
-                );
+            
+            //Only save the laws we are interested in
+            $whitelist = array('CC', 'CCP', 'CHC', 'CJP', 'CA', 'CCRP', 'CE', 'RS', 'CONST');
+            
+            $b = explode(' ', $sortcode);
+            if (!in_array($b[0], $whitelist)){
+                echo "\nSortcode $sortcode was skipped \n";
+                $law->clear(); 
+                unset($law);
+            } else {
+                //Put data in db
+                $q = $dbh->prepare("INSERT INTO `laws` (`id`, `docid`, `title`,
+                `description`, `sortcode`, `law_text`,`last_scraped`) 
+                VALUES (NULL, :docid, :title, :description, :sortcode, :law_text, 
+                CURRENT_TIMESTAMP);");
+        
+                $data = array(
+                    'docid' => $min,
+                    'title' => $title,
+                    'description' => $description,
+                    'sortcode' => $sortcode,
+                    'law_text' => $body_html
+                    );
 
-            //$q->execute($data);
+                $q->execute($data);
 
-            $error = $q->errorInfo();
-            if ($error[1])
-            {
-                print_r($error);$errors++;
+                $error = $q->errorInfo();
+                if ($error[1]) {
+                    print_r($error);$errors++;
+                } else {
+                    $counter++;
+                }
+
+                $law->clear(); 
+                unset($law);
             }
-            else
-            {
-                $counter++;
-            }
-
-            $law->clear(); 
-            unset($law);
         }
     } 
 }
+
+//Now remove duplicates;
+$q = $dbh->prepare('ALTER IGNORE TABLE laws ADD UNIQUE (title, id)');
+$q->execute();
 
 //Find execution time
 $time_end = microtime(true);
